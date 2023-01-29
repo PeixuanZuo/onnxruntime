a706f24a9c3dc061f4e67f1c4d7bf87397c18201
Yi Zhang <zhanyi@microsoft.com>
2023-01-29 11:25:50 +0800

    try GCC diagnostic

--- include/onnxruntime/core/framework/to_tensor_proto_element_type.h
+++ include/onnxruntime/core/framework/to_tensor_proto_element_type.h
@@ -7,7 +7,10 @@
 #include <string>
 
 #ifndef SHARED_PROVIDER
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include "onnx/onnx_pb.h"
+#pragma GCC diagnostic pop
 #endif
 
 #include "core/framework/float16.h"
